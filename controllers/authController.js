4f8ce3ba081068d07aec5f5b94ff0cd63eb58c0c
rifal12330 <rifalmuhamad12330@gmail.com>
2024-12-04 20:16:57 +0700

    second commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 // authController.js
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { createUser, findUserByEmail } = require('../models/userModel');  // Import fungsi dari model
+const { createUser, findUserByEmail } = require('../models/userModel'); // Import fungsi dari model
 
 // Register
 const register = async (req, res) => {
@@ -13,13 +13,13 @@ const register = async (req, res) => {
     // Simpan pengguna baru ke database
     createUser(name, email, hashedPassword, (err, result) => {
       if (err) {
-        return res.status(500).json({ message: 'Error registering user' });
+        return res.status(500).json({ error: true, message: 'Error registering user' });
       }
-      res.status(201).json({ message: 'User registered successfully' });
+      res.status(201).json({ error: false, message: 'User registered successfully' });
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error registering user' });
+    res.status(500).json({ error: true, message: 'Error registering user' });
   }
 };
 
@@ -30,28 +30,37 @@ const login = async (req, res) => {
     // Cari user berdasarkan email
     findUserByEmail(email, async (err, user) => {
       if (err || !user) {
-        return res.status(400).json({ message: 'User not found' });
+        return res.status(400).json({ error: true, message: 'User not found' });
       }
 
       // Bandingkan password yang dimasukkan dengan hash password di database
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
-        return res.status(400).json({ message: 'Invalid credentials' });
+        return res.status(400).json({ error: true, message: 'Invalid credentials' });
       }
 
       // Buat token JWT jika login berhasil
-      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      res.json({ message: 'Login successful', token });
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+      res.json({
+        error: false,
+        message: 'Login successful!',
+        loginResult: {
+          userId: user.id,
+          name: user.name,
+          token,
+        },
+      });
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error logging in' });
+    res.status(500).json({ error: true, message: 'Error logging in' });
   }
 };
 
 // Logout
 const logout = (req, res) => {
-  res.json({ message: 'Logged out successfully' });
+  res.json({ error: false, message: 'Logged out successfully' });
 };
 
 module.exports = { register, login, logout };
