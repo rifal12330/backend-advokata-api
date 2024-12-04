ed597dca07e54cdb97e4b90b0e3bebd0512cd752
rifal12330 <rifalmuhamad12330@gmail.com>
2024-12-04 18:55:57 +0700

    first commit

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
-// db.js
 const mysql = require('mysql2');
 
 // Membuat koneksi dengan Cloud SQL menggunakan Unix socket
 const connection = mysql.createConnection({
-  host: process.env.CLOUD_SQL_CONNECTION_NAME,  // Cloud SQL instance
-  user: process.env.DB_USER,                    // Username database
-  password: process.env.DB_PASSWORD,            // Password database
-  database: process.env.DB_NAME,                // Nama database
-  socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`, // Cloud SQL connection name
+  user: process.env.DB_USER,                  // Database user
+  password: process.env.DB_PASSWORD,          // Database password
+  database: process.env.DB_NAME,              // Database name
+  socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`, // Cloud SQL connection path
 });
 
 // Memastikan koneksi berhasil
